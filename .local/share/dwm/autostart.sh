6e896d7d5628a6352e63c2257e7c7d1f77fe23f2
lazar <lazuskica@gmail.com>
2025-01-19 11:18:40 +0100

    toggle_kbd na brojeve

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -10,4 +10,4 @@ gammastep -l 45.2:19.7 -t 5700:3600 -g 0.8 -m randr -v &
 xrandr --output HDMI-A-0 --mode 1920x1080 -r 100 &
 laptop_mon_startup.sh &
 dunst &
-#mpd &
+setxkbmap -layout "us,rs,rs" -variant ",basic,latin" -option "grp:alt_shift_toggle"
